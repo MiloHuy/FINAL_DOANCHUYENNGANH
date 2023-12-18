05da0b68a994f1036e23cf4a214e9eac8e993c36
phuong090802 <20110547@student.hcmute.edu.vn>
2023-12-18 21:50:58 +0700

    Update status code auth middleware, search topic

--- BE/app.js
+++ BE/app.js
@@ -1,21 +1,24 @@
-import express from "express";
-import admin from "./routes/admin.js";
-import auth from "./routes/auth.js";
-import topic from "./routes/topic.js";
-import user from "./routes/user.js";
-
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import dotenv from "dotenv";
-import { authorizeRoles, isAuthenticatedUser } from "./middlewares/auth.js";
+import express from 'express';
+import admin from './routes/admin.js';
+import auth from './routes/auth.js';
+import user from './routes/user.js';
+import topic from './routes/topic.js';
+import upload from './routes/upload.js';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import {
+  isAuthenticatedUser,
+  authorizeRoles,
+} from './middlewares/auth.js';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const corsOptions = {
   origin: process.env.FRONTEND_URL,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
 
@@ -26,10 +29,13 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use("/api/v1/topics", topic);
-app.use("/api/v1/admin", isAuthenticatedUser, authorizeRoles("admin"), admin);
+
+app.use('/api/v1/topics', topic);
+app.use('/api/v1/admin', isAuthenticatedUser, authorizeRoles('admin'), admin);
 // app.use('/api/v1/admin', admin);
-app.use("/api/v1/users", user);
-app.use("/api/v1/auth", auth);
+app.use('/api/v1/users', user);
+app.use('/api/v1/auth', auth);
+
+app.use('/api/v1/uploads', isAuthenticatedUser, authorizeRoles('user'), admin, upload);
 
 export default app;
